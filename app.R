e18c98178b1f15103bc0fdaafb6b7b7444f71793
Miriam <fernando.perez8@um.es>
2020-04-01 18:57:14 +0200

    texto

--- app.R
+++ app.R
@@ -6,8 +6,8 @@ library(shinyWidgets)
 library(shinyjs)
 source("global.R")
 ui <- dashboardPage(
-  dashboardHeader(title = "RNA-seq web utils", titleWidth = "300px"),
-  dashboardSidebar(disable = FALSE),
+  dashboardHeader(title = "Multiomics web utils", titleWidth = "300px"),
+  dashboardSidebar(disable = TRUE),
   dashboardBody(
       setShadow(class = "box"),
       tags$head(tags$style(HTML('
@@ -20,12 +20,16 @@ ui <- dashboardPage(
             useShinyjs(),
       fluidRow(
           miBoxPlus(
-              title = "DESeq enrichment",
+              title = "Simple Enrichment",
               closable = FALSE,
               width = 4,
               background = "limon",
-              height = "200px",
-              tags$h4("aquí una breve descripción de esta app"),
+              height = "250px",
+              tags$p("Enter a single list of genes or proteins in one of the different annotation format allowed 
+                     and the integrated species to get a full functional enrichment analysis based 
+                     on Kyoto Encyclopedia of Genes and Genomes (", a("Kegg", href="https://www.genome.jp/kegg/pathway.html"), 
+                     ") pathways database, Gene Ontology terms resource (", a("GO", href="http://geneontology.org"), ") 
+                     and Gene Set Enrichment Analysis (", a("GSEA", href="https://www.gsea-msigdb.org/gsea/index.jsp"),")."),
               tags$br(),
               fluidRow(
                   column(
@@ -33,12 +37,110 @@ ui <- dashboardPage(
                       width = 2,
                       dropdown(
                           style = "unite",
-                          icon = icon("gear"),
+                          icon = icon("info"),
                           status = "primary",
                           width = "600px",
                           animate = animateOptions(
                               enter = animations$fading_entrances$fadeInLeftBig,
                               exit = animations$fading_exits$fadeOutRightBig
+                          ),
+                          box(
+                              width = 12,
+                              status = "primary",
+                              height = "300px",
+                              title = tags$strong("Enrich app 2020"),
+                              background = "light-blue",
+                              tags$h4(
+                                  "Format allowed: bla, bla, bla.
+                                  Species integrated: Human and Mouse (Rat ?)."
+                              )
+                          )
+                      ),# fin dropdown
+                  ),#fin column
+                  column(
+                      width = 8,
+                      align = "center",
+                      actionBttn(
+                          inputId = "simple",
+                          label = "Go Simple Enrich",
+                          style = "jelly",
+                          color = "primary",
+                      )
+                  ) #fin column
+              ) # fin fluid row
+          ), # fin miboxplus 1
+          miBoxPlus(
+              title = "Count importer & DE analysis",
+              closable = FALSE,
+              width = 4,
+              background = "limon",
+              height = "250px",
+              tags$p("Create a DESeq2 object starting from the counting matrix of your transcriptomics experiment. Add a file with the 
+                     samples description, insert your design and the contrast of interest that you would like to test 
+                     and get an object ready for differential expression gene analysis and enrichment exploration."),
+              tags$br(),
+              fluidRow(
+                  column(
+                      align = "center",
+                      width = 2,
+                      dropdown(
+                          style = "unite",
+                          icon = icon("info"),
+                          status = "primary",
+                          width = "600px",
+                          animate = animateOptions(
+                              enter = animations$fading_entrances$fadeInDownBig,
+                              exit = animations$fading_exits$fadeOutDownBig
+                          ),
+                          box(
+                              width = 12,
+                              status = "primary",
+                              height = "300px",
+                              title = tags$strong("Enrich app 2020"),
+                              background = "light-blue",
+                              tags$h4(
+                                  "The accepted format for the counting file is a .csv file in which 
+                                  the columns are samples and the rows are genes."
+                              )
+                          )
+                      ), # fin dropdown
+                  ), #fin column
+                  column(
+                      width = 8,
+                      align = "center",
+                      actionBttn(
+                          inputId = "import",
+                          label = "Go importer & DE",
+                          style = "jelly",
+                          color = "primary",
+                      )
+                  ) #fin column
+              ) # fin fluid row
+          ), # fin miboxplus 2
+          miBoxPlus(
+              title = "DESeq enrichment",
+              closable = FALSE,
+              width = 4,
+              background = "limon",
+              height = "250px",
+              tags$p("Enter a DESeq2 object (created by yourself or by using the 'Go importer & DE' tools, indicate the 
+                     annotation and the specie and generate a full functional enrichment analysis based 
+                     on Kyoto Encyclopedia of Genes and Genomes (", a("Kegg", href="https://www.genome.jp/kegg/pathway.html"), 
+                     ") pathways database, Gene Ontology terms resource (", a("GO", href="http://geneontology.org"), ") 
+                     and Gene Set Enrichment Analysis (", a("GSEA", href="https://www.gsea-msigdb.org/gsea/index.jsp"),")."),
+              tags$br(),
+              fluidRow(
+                  column(
+                      align = "center",
+                      width = 2,
+                      dropdown(
+                          style = "unite",
+                          icon = icon("info"),
+                          status = "primary",
+                          width = "600px",
+                          animate = animateOptions(
+                              enter = animations$fading_entrances$fadeInRightBig,
+                              exit = animations$fading_exits$fadeOutLeftBig
                           ),
                           box(
                               width = 12,
@@ -61,98 +163,6 @@ ui <- dashboardPage(
                       actionBttn(
                           inputId = "enrich",
                           label = "Go DESeq EnrichApp",
-                          style = "jelly",
-                          color = "primary",
-                      )
-                  ) #fin column
-              ) # fin fluid row
-          ), # fin miboxplus 1
-          miBoxPlus(
-              title = "Simple Enrichment",
-              closable = FALSE,
-              width = 4,
-              background = "limon",
-              height = "200px",
-              tags$p("aquí una breve descripción de esta app"),
-              tags$br(),
-              fluidRow(
-                  column(
-                      align = "center",
-                      width = 2,
-                      dropdown(
-                          style = "unite",
-                          icon = icon("gear"),
-                          status = "primary",
-                          width = "600px",
-                          animate = animateOptions(
-                              enter = animations$fading_entrances$fadeInDownBig,
-                              exit = animations$fading_exits$fadeOutDownBig
-                          ),
-                          box(
-                              width = 12,
-                              status = "primary",
-                              height = "300px",
-                              title = tags$strong("Enrich app 2020"),
-                              background = "light-blue",
-                              tags$h4(
-                                  "To use this app is necesary a deseq2 object with
-                                  differential expresion computed. blah blah"
-                              )
-                          )
-                      ),# fin dropdown
-                  ),#fin column
-                  column(
-                      width = 8,
-                      align = "center",
-                      actionBttn(
-                          inputId = "simple",
-                          label = "Go Simple Enrich",
-                          style = "jelly",
-                          color = "primary",
-                      )
-                  ) #fin column
-              ) # fin fluid row
-          ), # fin miboxplus 2
-          miBoxPlus(
-              title = "Count importer & DE analysis",
-              closable = FALSE,
-              width = 4,
-              background = "limon",
-              height = "200px",
-              tags$p("aquí una breve descripción de esta app"),
-              tags$br(),
-              fluidRow(
-                  column(
-                      align = "center",
-                      width = 2,
-                      dropdown(
-                          style = "unite",
-                          icon = icon("gear"),
-                          status = "primary",
-                          width = "600px",
-                          animate = animateOptions(
-                              enter = animations$fading_entrances$fadeInRightBig,
-                              exit = animations$fading_exits$fadeOutLeftBig
-                          ),
-                          box(
-                              width = 12,
-                              status = "primary",
-                              height = "300px",
-                              title = tags$strong("Enrich app 2020"),
-                              background = "light-blue",
-                              tags$h4(
-                                  "To use this app is necesary a deseq2 object with
-                                  differential expresion computed. blah blah"
-                              )
-                          )
-                      ), # fin dropdown
-                  ), #fin column
-                  column(
-                      width = 8,
-                      align = "center",
-                      actionBttn(
-                          inputId = "import",
-                          label = "Go importer & DE",
                           style = "jelly",
                           color = "primary",
                       )
